e3e651ff4238833b5d560b5346024609c6b07277
czy0729 <402731062@qq.com>
2020-04-21 00:46:20 +0800

    update 2020/04/21

--- discovery.js
+++ discovery.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2020-04-05 03:20:34
  * @Last Modified by: czy0729
- * @Last Modified time: 2020-04-05 04:21:54
+ * @Last Modified time: 2020-04-21 00:37:25
  */
 const fs = require('fs')
 const path = require('path')
@@ -14,12 +14,16 @@ const fetch = require('./utils/old-fetch')
 async function downloadImage(cover, subjectId) {
   return new Promise((resolve, reject) => {
     const filePath = `./data/discovery/lain/${subjectId}.jpg`
+    if (fs.existsSync(filePath)) {
+      console.log(`- skip ${filePath}`)
+      return resolve(true)
+    }
 
     const src = `http:${cover}`
     http.get(src, (req, res) => {
       let imgData = ''
       req.setEncoding('binary')
-      req.on('data', chunk => (imgData += chunk))
+      req.on('data', (chunk) => (imgData += chunk))
       req.on('end', () => {
         const dirPath = path.dirname(filePath)
         if (!fs.existsSync(dirPath)) {
@@ -27,7 +31,7 @@ async function downloadImage(cover, subjectId) {
         }
 
         console.log(`- write ${cover}.jpg`)
-        fs.writeFileSync(filePath, imgData, 'binary', err => {
+        fs.writeFileSync(filePath, imgData, 'binary', (err) => {
           if (err) console.log('- error ${avatar}')
         })
 
@@ -45,18 +49,18 @@ async function fetchHome() {
   const filePath = './data/discovery/index.json'
 
   const covers = []
-  Object.keys(data).forEach(key => {
+  Object.keys(data).forEach((key) => {
     if (Array.isArray(data[key])) {
-      data[key].forEach(item => {
+      data[key].forEach((item) => {
         covers.push({
           subjectId: item.subjectId,
-          cover: utils.getCoverLarge(item.cover)
+          cover: utils.getCoverLarge(item.cover),
         })
         item.cover = `https://cdn.jsdelivr.net/gh/czy0729/Bangumi-Static@master/data/discovery/cover/${item.subjectId}.jpg`
       })
     }
   })
-  const fetchs = covers.map(item => () =>
+  const fetchs = covers.map((item) => () =>
     downloadImage(item.cover, item.subjectId)
   )
   utils.queue(fetchs, 2)
